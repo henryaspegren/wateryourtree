836d1103dce4bb31a7aec703a0fbcaa5f2e4242c
Mat Geist <matgeist@gmail.com>
2014-05-27 11:10:17 -0700

    added a pm of emoticon + location to hipchat user

--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 
 from core.models import Location
 
+import hypchat
+
+hc = hypchat.HypChat("HMKmVOnIKudLTskiuZfosszhed4cRX4S9TeFb3s2")
+
 # Create your views here.
 def index(request):
   locations = Location.objects.order_by('-date_created')
@@ -10,5 +14,9 @@ def index(request):
 
 def detail(request, location_url):
   location = get_object_or_404(Location, url=location_url)
+
+  me = hc.get_user("mgeist@betterworks.com")
+  me.message("(poo) - " + location.name)
+
   context = {'location': location}
   return render(request, 'core/detail.html', context)
