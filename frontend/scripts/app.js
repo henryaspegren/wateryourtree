a38d2f6c837a47d7f65ac02798d2d03bc1eb8ffa
Henry Aspegren <henry@betterworks.com>
2014-06-09 11:50:13 -0700

    Mapping functionality added

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,18 +1,36 @@
-var fertilizer = angular.module("fertilizer", ['ngRoute','ja.qr', 'locationsController']);
+var fertilizer = angular.module("fertilizer", ['ja.qr', 'google-maps'])
+  .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+  }])
+  .controller('dataController', function ($scope, $http){
+    // URL for the QR code to generate
+    $scope.qr_url = "";
+    $scope.test = "data is finally binding";
+    $scope.selected_location = false;
+    $scope.location = "";
+    $scope.map = {
+      center: {
+        latitude: 37.42565,
+        longitude: -122.13535
+      },
+      zoom: 16
+    };
 
-fertilizer.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/fertilizer/location', {
-        templateUrl: 'location1.html',
-        controller: 'locationsController'
-      }).
-      when('fertilizer/create', {
-        templateUrl: 'create.html'
-        controller: 'locationsController'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-    });
-}]);
+    $scope.get_locations = function () {
+      $http.get("../core/list")
+        .success(function (data, status, headers, config) {
+          $scope.locations = data;
+        });
+    };
+    $scope.create_location = function(name) {
+      $http.post("../core/create/", "Location=" + name)
+        .success(function (data, status, headers, config) {
+          window.location = "/fertilizer/location.html";
+        });
+    };
+    $scope.select_location = function(location) {
+      $scope.selected_location =  true;
+      $scope.location = location;
+      $scope.qr_url = document.domain + "/core/hit/" + $scope.location.url;
+    };
+});
