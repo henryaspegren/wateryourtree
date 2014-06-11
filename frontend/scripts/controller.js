82575c0038a3bb080bd29ac6a82ff4a785485b92
Henry Aspegren <henry@betterworks.com>
2014-06-11 15:00:01 -0700

    Tree editing and creation added

--- frontend/scripts/controller.js
+++ frontend/scripts/controller.js
@@ -1,6 +1,10 @@
 'use strict';
 var fertilizerControllers = angular.module("fertilizerControllers", ['ja.qr', 'google-maps']);
 
+fertilizerControllers.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+  }])
+
 fertilizerControllers.controller('locationsController', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
     $scope.url = $routeParams.url;
@@ -26,13 +30,21 @@ fertilizerControllers.controller('locationsController', ['$scope', '$routeParams
         });
       $scope.qr_url = document.domain + "/core/hit/" + url
     };
+    $scope.update_latitude=function(url, lat) {
+      $http.post("../core/updatelatitude/","url_link="+url+";lat_update="+lat)
+    };
+
+    $scope.update_longitude=function(url,lng) {
+      $http.post("../core/updatelongitude/", "url_link="+url+";lng_update="+lng)
+    };
 
     $scope.create_location = function(name) {
-      $http.post("../core/create/", "Location=" + name)
-        .success(function (data, status, headers, config) {
-          window.location = "/fertilizer/index.html";
+      $http.post("../core/create/", "Location=" +name)
+       .success(function (data, status, headers, config) {
+          window.location = '/fertilizer/index.html#/create';
+          $scope.tree = name
         });
-    };
+     };
     $scope.select_location = function(location) {
       $scope.location = location;
       $scope.qr_url = document.domain + "/core/hit/" + $scope.location.url;
