3f7e6fdd8b75df9c228086f90df6997fe7f3be2d
henryaspegren <henrya@mit.edu>
2014-06-26 16:34:48 -0700

    heroku database settings changed

--- Fertilizer/settings.py
+++ Fertilizer/settings.py
@@ -56,12 +56,8 @@ WSGI_APPLICATION = 'Fertilizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -88,7 +84,3 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'Fertilizer/templates')
 )
-
-# UN COMMENT FOR HEROKU DEPLOYMENTS
-# # Parse database configuration from $DATABASE_URL
-# DATABASES['default'] =  dj_database_url.config()
